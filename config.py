895dc34c0ed882cdc64bf85d3fff927863878e80
Danishzain05 <163726214+Danishzain05@users.noreply.github.com>
2024-05-16 10:48:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "6440277281"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Heartwritez_09")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "StatusWorld_05")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "StatusWorld_05")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "Heartwritez_09")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "MafiaXD_05")
 
 # Random Start Images
