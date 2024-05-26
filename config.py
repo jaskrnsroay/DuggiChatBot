6e8eb32b78dcbfd36118ce6110f7d9c57f99fb93
𝐉𝐀𝐒𝐒 <150457649+jaskrnsroay@users.noreply.github.com>
2024-05-25 21:45:44 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+API_ID = int(getenv("API_ID", 20617772))
+API_HASH = getenv("API_HASH", "eb464a3d12219bbfbbbf2d4deb3b3151")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "6440277281"))
+OWNER_ID = int(getenv("OWNER_ID", "6527727109"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "StatusWorld_05")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "StatusWorld_05")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ZainXd_05")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "DilwaleHaven")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "DugguWorld")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "imceobitxh")
 
 # Random Start Images
 IMG = [
